a301a4dd0a70a917ded5e1e62dcf8711b7ad97a8
Keyvan Fatehi <keyvanfatehi@gmail.com>
2022-05-01 15:08:16 -0700

    basic wasd accel

--- tools/joystick/web.py
+++ tools/joystick/web.py
@@ -18,13 +18,70 @@ index = """
 <script type="text/javascript">
 // Create JoyStick object into the DIV 'joyDiv'
 var joy = new JoyStick('joyDiv');
-setInterval(function(){
-  var x = -joy.GetX()/100;
-  var y = joy.GetY()/100;
+function sendMoveCommand(x,y) {
   let xhr = new XMLHttpRequest();
   xhr.open("GET", "/control/"+x+"/"+y);
   xhr.send();
-}, 50);
+}
+function joyFunction(){
+  var x = -joy.GetX()/100;
+  var y = joy.GetY()/100;
+  sendMoveCommand(x,y);
+}
+var joyInterval = null;
+function startJoy() {
+    joyInterval = setInterval(joyFunction, 50);
+}
+function stopJoy() {
+    if (joyInterval) {
+        clearInterval(joyInterval);
+        joyInterval = null;
+    }
+}
+let lastKey = null;
+let lastValue = null;
+const accelerator = (k)=>{
+    if (lastKey != k) {
+        lastValue = 0.1;
+        lastKey = k;
+    }
+    return ()=>{
+        let a = lastValue*1.027;
+        let value = a < 0.89 ? a : lastValue;
+        lastValue = value;
+        return value;
+    }
+}
+function forward(a) {
+sendMoveCommand(0, a())
+}
+function left(a) {
+sendMoveCommand(a(), 0)
+}
+function right(a) {
+sendMoveCommand(-a(), 0)
+}
+function backward(a) {
+sendMoveCommand(0, -a())
+}
+function keyDownHandler() {
+    stopJoy();
+    if (!event) return;
+    let accel = accelerator(event.key);
+    if (event.key === 'w' || event.key === 'ArrowUp')
+        forward(accel);
+    else if (event.key === 's' || event.key === 'ArrowDown')
+        backward(accel);
+    else if (event.key === 'a' || event.key === 'ArrowLeft')
+        left(accel);
+    else if (event.key === 'd' || event.key === 'ArrowRight')
+        right(accel);
+    else
+        console.log('unhandled key:', event.key);
+    startJoy();
+}
+document.addEventListener('keydown', keyDownHandler, true);
+startJoy();
 </script>
 """
 
